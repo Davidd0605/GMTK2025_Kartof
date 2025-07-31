20baf3629ded1df33cafdab36a386c4407ea5e83
DamianLHr <damianhr@abv.bg>
2025-08-01 00:15:10 +0300

    Made time travel camera effect and integrated it in the scene controller

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -10,6 +10,7 @@ public class SceneController : MonoBehaviour
     [SerializeField] private bool canSwitch = true;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject camera;
+    [SerializeField] private TimeTravelEffect timeTravelEffect;
 
     // Update is called once per frame
     void Update()
@@ -23,8 +24,13 @@ public class SceneController : MonoBehaviour
 
     private IEnumerator LoadScene(int sceneIndex)
     {
+
         Debug.Log(sceneIndex);
         yield return new WaitForSeconds(timeSpentInScene);
+
+        timeTravelEffect.TriggerTimeTravel();
+        yield return new WaitForSeconds(timeTravelEffect.effectDuration + timeTravelEffect.effectDisplacement);
+
         player.transform.position = new Vector2(player.transform.position.x, -3 + sceneIndex*50);
         camera.transform.position = new Vector3(camera.transform.position.x, sceneIndex*50, -10);
         if (sceneIndex < 2)
@@ -33,5 +39,6 @@ public class SceneController : MonoBehaviour
             sceneIndex = -2;
         nextScene = sceneIndex;
         canSwitch = true;
+
     }
 }
