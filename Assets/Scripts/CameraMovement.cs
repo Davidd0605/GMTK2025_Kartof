97bf4979f3c6ca784fc1499fe0f57d5caa1f3386
BungerColumbus <erceanu.vlad@gmail.com>
2025-08-01 00:18:23 +0300

    Made camera follow player

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float smoothing;
+    private Vector3 targetPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != player.position)
+        {
+            targetPosition = new Vector3(player.position.x, transform.position.y, -10);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, -3, 0);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
