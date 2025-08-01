274812b4d7233a4d8d2f27dddb7bb75a466c59ee
DamianLHr <damianhr@abv.bg>
2025-08-01 23:52:57 +0300

    Assembled all time instances, made changes to room transitions to accomodate

---
+++ Assets/Scripts/Rooms/RoomController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+
+    [SerializeField] private Transform cameraPosition;
+    [SerializeField] public Transform playerStartPoint;
+
+    public void TransportPlayer(GameObject player, Vector3 displacement)
+    {
+        player.transform.position = playerStartPoint.position + displacement;
+    }
+
+    public void TransportCamera(GameObject camera)
+    {
+        camera.transform.position = new Vector3(cameraPosition.position.x, cameraPosition.position.y, -10);
+    }
+}
