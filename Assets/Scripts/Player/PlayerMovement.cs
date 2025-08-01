ddb573058d3038d3bbab88be7660b154a6ef7c2d
BungerColumbus <erceanu.vlad@gmail.com>
2025-08-01 13:42:01 +0300

    Set and animated player sprites

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,20 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Animator anim;
     private Vector2 movement;
     private static PlayerMovement instance;
+
     void Update()
     {
         // Get horizontal input using the old Input system
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         movement = new Vector2(horizontalInput, 0f).normalized;
+        anim.SetInteger("Horizontal", (int)horizontalInput);
+        if (horizontalInput != 0f)
+            sprite.transform.localScale = new Vector2(horizontalInput * Mathf.Abs(sprite.transform.localScale.x), 
+                                                                        sprite.transform.localScale.y);
     }
 
     void FixedUpdate()
