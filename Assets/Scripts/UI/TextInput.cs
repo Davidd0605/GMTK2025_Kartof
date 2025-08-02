fbecd30aa6eb973eb2eb61ff700576767a977a29
BungerColumbus <erceanu.vlad@gmail.com>
2025-08-02 19:56:52 +0300

    Input Console

---
+++ Assets/Scripts/UI/TextInput.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TextInput : MonoBehaviour
+{
+    public void Check(string inputText)
+    {
+        if(inputText == "7472")
+        {
+            Debug.Log("YIPEEEEEEE");
+        }
+        else
+        {
+            Debug.Log(":(");
+        }
+    }
+
+}
